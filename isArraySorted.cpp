3128123c5c9d2aac48bcd25a87354b766deef067
Ankit77760 <ankitm7972@gmail.com>
2024-01-13 20:57:51 +0530

    is

---
+++ isArraySorted.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+//tc-O(n) sc-O(1)
+string isArraySorted(int arr[],int n){
+    for(int i=1;i<n-1;i++){
+        if(arr[i-1]<=arr[i]){
+
+        }
+        else{
+            return "Not Sorted";
+        }
+    }
+    return "Sorted";
+}
+
+int main(){
+    int n;
+    cout<<"Enter N: ";
+    cin>>n;
+    int arr[n];
+
+    for(int i=0;i<n;i++){
+        cout<<"Enter "<<i<<" Element: ";
+        cin>>arr[i];
+    }
+
+    cout<<isArraySorted(arr,n);
+
+}
