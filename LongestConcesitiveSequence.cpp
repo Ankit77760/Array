37f525f34ac0f9e6c3896c682ef9c3d6930a08f9
Ankit77760 <ankitm7972@gmail.com>
2024-01-21 19:48:38 +0530

    LongestConcesitiveSequence

--- LongestConcesitiveSequence.cpp
+++ LongestConcesitiveSequence.cpp
@@ -11,6 +11,7 @@ int linearSearch(vector<int>&a,int num){
     return false;
 }
 
+//tc-O(N^2) sc-O(1);
 int longestSuccessiveElements(vector<int>&arr) {
     int n=arr.size();
     int longest=1;
@@ -25,6 +26,53 @@ int longestSuccessiveElements(vector<int>&arr) {
     }
 
     cout<<longest;
+}
+
+//tc-O(NlogN)+O(N) sc-O(1)
+int longestSuccessiveElementss(vector<int>&arr){
+    int n=arr.size();
+    sort(arr.begin(),arr.end());
+    int count=0;
+    int smaller=INT_MIN;
+    int longest=1;
+    for(int i=0;i<n;i++){
+        if(arr[i]-1==smaller){
+            count++;
+            smaller=arr[i];
+        }
+        else if(arr[i]!=smaller){
+            count=0;
+            smaller=arr[i];
+        }
+        longest=max(longest,count);
+    }
+    return longest;
+}
+
+//optimal solution tc-O(3N) sc-O(N)
+int longestSuccessiveElementsss(vector<int>&arr){
+    int n=arr.size();
+    if(n==0) return 0;
+
+    int longest=1;
+    unordered_set<int>st;
+    for(int i=0;i<n;i++){
+        st.insert(arr[i]);
+    }
+
+    for(auto &it:st){
+        if(st.find(it-1)==st.end()){
+            int cnt=1;
+            int x=it;
+            while(st.find(x+1)!=st.end()){
+                x=x+1;
+                cnt++;
+            }
+            longest=max(longest,cnt);
+        }
+    }
+    return longest;
+
 }
 
 int main(){
