139faa3a137a157189fbf646be66ef5554ffdbae
Ankit77760 <ankitm7972@gmail.com>
2024-01-26 20:10:50 +0530

    MajorityElementII

--- MajorityElementII.cpp
+++ MajorityElementII.cpp
@@ -71,7 +71,18 @@ int MajorityElementtt(int arr[],int n){
             cnt2--;
         }
     }
+    vector<int>ans;
+    int cnt1=0;
+    int cnt2=0;
+    for(int i=0;i<n;i++){
+        if(el1==arr[i]) cnt1++;
+        if(el2==arr[i]) cnt2++;
+    }
+    int mini=int(n/3)+1;
+    if(cnt1>mini) ans.push_back(el1);
+    if(cnt2>mini) ans.push_back(el2);
 
+    return ans;
 }
 
 int main(){
